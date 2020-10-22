0d4b9729935fe37641f42eb145f016993e316361
Saumya Saxena <saumyasaxena@ip-192-168-1-6.ec2.internal>
2020-10-22 17:50:57 +0530

    Fix Test User

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stargate-utils",
-    version="1.0.0",
+    version="1.0.1",
     description="Utils package commonly used by microservices",
     long_description=long_description,
     long_description_content_type="text/markdown",
